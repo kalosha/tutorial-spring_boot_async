777d2abfcc1a7c9143e5ce0227b36607ef863115
Mikheil Kalandadze <kalosha@gmail.com>
2024-05-04 12:02:50 -0400

    init

---
+++ DownstreamServer/src/test/java/com/epam/community/downstreamserver/DownstreamServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.epam.community.downstreamserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DownstreamServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
